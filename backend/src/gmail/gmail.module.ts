560537ddfb5e8148dda91327c11193e8542dadfc
Sai Puneeth <saipuneeth2004@gmail.com>
2025-11-02 12:07:02 +0530

    reset

--- backend/src/gmail/gmail.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GmailController } from './gmail.controller';
-import { GmailService } from './gmail.service';
-
-@Module({
-  controllers: [GmailController],
-  providers: [GmailService],
-})
-export class GmailModule {}
